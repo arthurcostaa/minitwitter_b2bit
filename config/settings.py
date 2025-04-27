61a011b4b9287ca51698515f60fdc3a572d320a4
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2025-04-27 14:33:03 -0300

    Add custom user model

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -84,3 +85,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
