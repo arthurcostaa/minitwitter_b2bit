02a45a1592900a2a823280bafc6e64e6125d3742
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2025-04-28 20:24:19 -0300

    Add IsOwnerOrReadOnly permission

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import CustomUser
+from .permissions import IsOwnerOrReadOnly
 from .serializers import CustomUserSerializer
 
 
@@ -10,4 +11,4 @@ class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all().order_by('username')
     serializer_class = CustomUserSerializer
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
