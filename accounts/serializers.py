4a399a431613cb021aad2d756ffb1c2fe7565e01
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2025-04-27 22:01:05 -0300

    Add password validation

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
 from rest_framework import serializers
 
 from .models import CustomUser
@@ -5,19 +7,6 @@ from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
-
-    class Meta:
-        model = CustomUser
-        fields = [
-            'id',
-            'username',
-            'email',
-            'first_name',
-            'last_name',
-            'password',
-            'date_joined',
-        ]
-        read_only_fields = ['date_joined']
 
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
@@ -31,3 +20,31 @@ class CustomUserSerializer(serializers.ModelSerializer):
         instance.set_password(password)
         instance.save()
         return instance
+
+    def validate(self, data):
+        user = CustomUser(**data)
+        password = data.get('password')
+        errors = {}
+
+        try:
+            validate_password(password, user)
+        except exceptions.ValidationError as e:
+            errors['password'] = list(e.messages)
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return data
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            'id',
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'password',
+            'date_joined',
+        ]
+        read_only_fields = ['date_joined']
