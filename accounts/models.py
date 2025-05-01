65aafcada78e867cf136663104af5c5c005d844f
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2025-04-30 21:29:51 -0300

    Add follow and unfollow endpoints

--- accounts/models.py
+++ accounts/models.py
@@ -5,9 +5,23 @@ from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     date_joined = models.DateField(auto_now_add=True)
+    following = models.ManyToManyField(
+        'self', related_name='followers', symmetrical=False
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.username
+
+    def is_following(self, user):
+        return self.following.filter(pk=user.pk).exists()
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.following.add(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import CustomUser
@@ -18,3 +21,37 @@ class CustomUserViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
         return [permission() for permission in permission_classes]
+
+    @action(detail=True, methods=['post'], url_path='follow', url_name='follow')
+    def follow(self, request, pk=None):
+        user = get_object_or_404(CustomUser, pk=self.kwargs['pk'])
+        current_user = self.request.user
+
+        if current_user == user:
+            return Response(
+                {'detail': "You can't follow yourself."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        current_user.follow(user)
+
+        return Response(
+            {'detail': 'User followed successfully.'}, status=status.HTTP_200_OK
+        )
+
+    @action(detail=True, methods=['delete'], url_path='unfollow', url_name='unfollow')
+    def unfollow(self, request, pk=None):
+        user = get_object_or_404(CustomUser, pk=self.kwargs['pk'])
+        current_user = self.request.user
+
+        if current_user == user:
+            return Response(
+                {'detail': "You can't unfollow yourself."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        current_user.unfollow(user)
+
+        return Response(
+            {'detail': 'User unfollowed successfully.'}, status=status.HTTP_200_OK
+        )
