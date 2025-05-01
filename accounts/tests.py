16c5a3bfebb7a62943407b1f118ae40c4cd44d15
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2025-05-01 18:28:45 -0300

    Add like, unlike tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from .models import CustomUser
+from accounts.models import CustomUser
+from posts.models import Post
 
 
 class CustomUserTest(TestCase):
@@ -11,6 +12,7 @@ class CustomUserTest(TestCase):
         self.user2 = CustomUser.objects.create_user(
             username='joao', email='joao@email.com', password='GoodMorning#123'
         )
+        self.post = Post.objects.create(author=self.user2, content='xyz')
 
     def test_follow(self):
         self.user1.follow(self.user2)
@@ -22,8 +24,24 @@ class CustomUserTest(TestCase):
         self.assertFalse(self.user1.following.filter(pk=self.user2.pk).exists())
 
     def test_user_is_following_another_user(self):
-        self.user1.following.add(self.user2)
+        self.user1.follow(self.user2)
         self.assertTrue(self.user1.is_following(self.user2))
 
     def test_user_is_not_following_another_user(self):
         self.assertFalse(self.user1.is_following(self.user2))
+
+    def test_like(self):
+        self.user1.like(self.post)
+        self.assertTrue(self.user1.likes.filter(pk=self.post.pk).exists())
+
+    def test_unlike(self):
+        self.user1.likes.add(self.post)
+        self.user1.unlike(self.post)
+        self.assertFalse(self.user1.likes.filter(pk=self.post.pk).exists())
+
+    def test_user_liked_post(self):
+        self.user1.like(self.post)
+        self.assertTrue(self.user1.is_liked(self.post))
+
+    def test_user_dont_like_post(self):
+        self.assertFalse(self.user1.is_liked(self.post))
