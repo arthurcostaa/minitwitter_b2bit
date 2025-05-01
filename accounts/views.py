32a47fd7955a8d47edaa0d4ef753303883e09a6f
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2025-05-01 13:16:14 -0300

    Refactor follow and unfollow actions in user viewset

--- accounts/views.py
+++ accounts/views.py
@@ -22,36 +22,24 @@ class CustomUserViewSet(viewsets.ModelViewSet):
             permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
         return [permission() for permission in permission_classes]
 
-    @action(detail=True, methods=['post'], url_path='follow', url_name='follow')
+    @action(
+        detail=True, methods=['post', 'delete'], url_path='follow', url_name='follow'
+    )
     def follow(self, request, pk=None):
         user = get_object_or_404(CustomUser, pk=self.kwargs['pk'])
         current_user = self.request.user
 
         if current_user == user:
-            return Response(
-                {'detail': "You can't follow yourself."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+            if request.method == 'POST':
+                data = {'detail': "You can't follow yourself."}
+            elif request.method == 'DELETE':
+                data = {'detail': "You can't unfollow yourself."}
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
-        current_user.follow(user)
-
-        return Response(
-            {'detail': 'User followed successfully.'}, status=status.HTTP_200_OK
-        )
-
-    @action(detail=True, methods=['delete'], url_path='unfollow', url_name='unfollow')
-    def unfollow(self, request, pk=None):
-        user = get_object_or_404(CustomUser, pk=self.kwargs['pk'])
-        current_user = self.request.user
-
-        if current_user == user:
-            return Response(
-                {'detail': "You can't unfollow yourself."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        current_user.unfollow(user)
-
-        return Response(
-            {'detail': 'User unfollowed successfully.'}, status=status.HTTP_200_OK
-        )
+        if request.method == 'POST':
+            current_user.follow(user)
+            data = {'detail': 'User followed successfully.'}
+        elif request.method == 'DELETE':
+            current_user.unfollow(user)
+            data = {'detail': 'User unfollowed successfully.'}
+        return Response(data, status=status.HTTP_200_OK)
